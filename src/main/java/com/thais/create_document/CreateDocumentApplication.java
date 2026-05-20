349ed49a390bbe954e7ce42e853fae918cfeffa0
Thaís Alcantara <tatie.alcantara@gmail.com>
2026-05-20 20:56:03 -0300

    start

---
+++ src/main/java/com/thais/create_document/CreateDocumentApplication.java
@@ -0,0 +1,13 @@
+package com.thais.create_document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateDocumentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateDocumentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thais/create_document/CreateDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thais.create_document;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
