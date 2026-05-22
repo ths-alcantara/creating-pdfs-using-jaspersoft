fb8fa384a002a79ac0527bcb342763c44b91f0a0
Thaís Alcantara <tatie.alcantara@gmail.com>
2026-05-22 19:44:57 -0300

    add atributes to DTO

--- src/main/java/com/thais/create_document/dto/DocumentInfoDTO.java
+++ src/main/java/com/thais/create_document/dto/DocumentInfoDTO.java
@@ -3,8 +3,14 @@ package com.thais.create_document.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class DocumentInfoDTO {
-
+    private String name;
+    private Date dataFinalizacao;
+    private String cpf;
+    private String course;
+    private String professor;
 }
